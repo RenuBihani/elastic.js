8c3f23d00d0637cf8e3b8b8e308b44a118b56612
Faustina Fechita <faustina.fechita@gmail.com>
2016-06-29 16:02:15 +0100

    Added WeightScoreFunction. This should be used instead of BoostFactorScoreFunction ad boost_factor is no longer available in ElasticSearch 2.x

--- dist/elastic.js
+++ dist/elastic.js
@@ -1,6 +1,6 @@
-/*! elastic.js - v1.2.0 - 2014-10-13
+/*! elastic.js - v1.2.0 - 2016-06-29
  * https://github.com/fullscale/elastic.js
- * Copyright (c) 2014 FullScale Labs, LLC; Licensed MIT */
+ * Copyright (c) 2016 FullScale Labs, LLC; Licensed MIT */
 
 /**
  @namespace
@@ -15247,6 +15247,53 @@
         return this;
       }
 
+
+    });
+  };
+
+  /**
+   @class
+     <p>The weight score allows you to multiply the score by the provided weight
+   This can sometimes be desired since boost value set on specific queries gets
+   normalized, while for this score function it does not.</p>
+
+   @name ejs.WeightScoreFunction
+   @ejs scorefunction
+   @borrows ejs.ScoreFunctionMixin.filter as filter
+   @borrows ejs.ScoreFunctionMixin._type as _type
+   @borrows ejs.ScoreFunctionMixin.toJSON as toJSON
+
+   @param {Float} weightVal the weight.
+
+   @desc
+   <p>Multiply the score by the provided weight.</p>
+
+   */
+  ejs.WeightScoreFunction = function (weightVal) {
+
+    var
+      _common = ejs.ScoreFunctionMixin('weight'),
+      func = _common.toJSON();
+
+    func.weight = weightVal;
+    
+    return extend(_common, {
+
+      /**
+       Sets the boost factor.
+
+       @member ejs.BoostFactorScoreFunction
+       @param {Float} b the boost factor.
+       @returns {Object} returns <code>this</code> so that calls can be chained.
+       */
+      weight: function (b) {
+        if (b == null) {
+          return func.weight;
+        }
+
+        func.weight = b;
+        return this;
+      }
 
     });
   };
